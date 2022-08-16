483e6a4fc81771e51ad2dc5223bbf2b6295e7bcc
sherxonDeveloper <sherxontoshpulatov3@gmail.com>
2022-08-16 18:38:14 +0500

    first commit

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-var money = prompt("Mablag'ingizni kiriting!")
+let money = prompt("Mablag'ingizni kiriting!");
 
-var dollar = 9433.34;
-var euro = 10354.03;
+let dollar = 11000.34;
+let euro = 12354.03;
 
-var toDollar = Number(money) / dollar;
-var toEuro = Number(money) / euro;
+let toDollar = Number(money) / dollar;
+let toEuro = Number(money) / euro;
 
 if (toDollar >= 500) {
-  if (toDollar - 500 >= 250){
-    var convert = ((toDollar - 750) * dollar) / euro
-    if(convert >= 120){
-      alert("Alisher sayohatga yaxshi borib keling.")
-    }else{
-      alert("Alisher sayohatga yaxshi borib keling likn Muzey bilan aylanishga yetmedi.")
+  if (toDollar - 500 >= 250) {
+    let convert = ((toDollar - 750) * dollar) / euro;
+    if (convert >= 120) {
+      alert("Alisher sayohatga yaxshi borib keling.");
+    } else {
+      alert(
+        "Alisher sayohatga yaxshi borib keling likn Muzey bilan aylanishga yetmedi."
+      );
     }
-  }else{
-    alert("Alisher pulingiz biletga yetdi lekin mehmonhonaga yetmaydi.")
+  } else {
+    alert("Alisher pulingiz biletga yetdi lekin mehmonhonaga yetmaydi.");
   }
 } else {
-  alert("Alisher pulingiz borib ketishga yetmaydi.")
+  alert("Alisher pulingiz borib kelishga yetmaydi.");
 }
